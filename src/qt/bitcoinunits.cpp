6d2ef0984616a930891cf290549883f77638594c
Otto Levin <odradek.online@gmail.com>
2013-12-25 02:22:52 +0100

    Name changed

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("TSC");
+    case mBTC: return QString("mTSC");
+    case uBTC: return QString::fromUtf8("μTSC");
     default: return QString("???");
     }
 }
